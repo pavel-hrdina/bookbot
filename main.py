e0aae4db830a1e549f6032ec841caf669d96ef17
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2025-03-29 18:21:56 +0100

    handle command line args

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     char_dic_to_sorted,
     count_word_occurrences,
@@ -12,13 +14,17 @@ def main() -> None:
     :return: None
     """
     # Check if the script received any arguments
-    book_content = get_book_text('./books/frankenstein.txt')
-    word_occurrences = count_word_occurrences(book_content)
-    new_char_list = char_dic_to_sorted(word_occurrences)
+    if len(sys.argv) > 1:
+        book_content = get_book_text(sys.argv[1])
+        word_occurrences = count_word_occurrences(book_content)
+        new_char_list = char_dic_to_sorted(word_occurrences)
 
-    length_of_string(book_content)
-    for char in new_char_list:
-        print(char)
+        length_of_string(book_content)
+        for char in new_char_list:
+            print(char)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
