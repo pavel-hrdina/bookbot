31c228d71bf9ffc94a47f27bde456b05fbf3ff1f
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2025-03-29 17:33:26 +0100

    add function that counts all the words in a given doc.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-def main():
-    return get_book_text('./books/frankenstein.txt')
+
+
+def main() -> None:
+    """
+    Main method of the program.
+    :return: None
+    """
+    # Check if the script received any arguments
+    book_content = get_book_text('./books/frankenstein.txt')
+    return length_of_string(book_content)
 
 
 def get_book_text(filepath) -> str:
+    """
+    Takes a filepath and returns its content.
+    :param filepath: str
+    :return: str
+    """
     with open(filepath) as f:
         return f.read()
 
 
+def length_of_string(text: str) -> None:
+    """
+    Takes a string and returns its length.
+    :param text: str
+    :return: None
+    """
+    length = len(text.split())
+    print(f"{length} words found in the document")
+
+
 if __name__ == '__main__':
-    print(main())
+    main()
 
