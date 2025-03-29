5793c973251266be79821d0bd9bb6ffa51c0e5ab
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2025-03-29 14:56:02 +0100

    read file function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def main():
+    return get_book_text('./books/frankenstein.txt')
+
+
+def get_book_text(filepath) -> str:
+    with open(filepath) as f:
+        return f.read()
+
+
+if __name__ == '__main__':
+    print(main())
+
